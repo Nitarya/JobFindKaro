9472d5821fd97d4ccf2c14f09b7c075ee40582ff
Nitya <niittss27@gmail.com>
2024-06-23 02:27:39 +0530

    Header prepared

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/NavBar";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <div>
         <Navbar />
+        <Header/>
       </div>
     </>
   );
